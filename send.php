38f83966f57da637fc69345b7bf249f365c5d251
bogaert <bogaert.david21@gmail.com>
2021-02-13 13:22:10 +0100

    formphp

---
+++ send.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_POST['submit'])) {
+
+$name = $_POST['name'];
+$mailFrom = $_POST['mail'];
+$message = $_POST['message'];
+
+$mailto = "belgianbogaert@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+
+mail($mailto, $txt, $headers);
+
+header("Location: index.php?mailsend");
+
+
+
+}
